c68ff29e9620e4723b4a2502b0d2dda36003983c
Mateusz <wosmateusz2@gmail.comm>
2022-09-01 17:35:40 +0200

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,37 @@
 const navBtn = document.querySelector('.burger-btn');
 const navBox = document.querySelector('.nav-box');
+const aboutMeText = document.querySelector('.section-body');
+const aboutMePhoto = document.querySelector('.aboutme-img');
 
 const openNav = () => {
-	if (!(navBox.classList.contains('nav-animation-open'))) {
+	if (!navBox.classList.contains('nav-animation-open')) {
 		navBox.classList.add('nav-animation-open');
 		navBox.classList.remove('nav-animation-close');
-	} else{
+	} else {
 		navBox.classList.remove('nav-animation-open');
 		navBox.classList.add('nav-animation-close');
 	}
 };
 
+const animationAboutText = () => {
+	if (window.scrollY >= 200) {
+		aboutMeText.classList.add('aboutme-animation-text');
+	}
+};
+
+const animationAboutPhoto = () => {
+	if (!window.matchMedia('(max-width: 1024px)').matches) {
+		if (window.scrollY >= 200) {
+			aboutMePhoto.classList.add('aboutme-animation-photo');
+		}
+	} else if (window.scrollY >= 1100) {
+		aboutMePhoto.classList.add('aboutme-animation-photo');
+	}
+};
+
+
 navBtn.addEventListener('click', openNav);
+window.addEventListener('scroll', animationAboutText);
+window.addEventListener('scroll', animationAboutPhoto);
+
+
